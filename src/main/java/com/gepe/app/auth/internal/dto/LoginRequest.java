1e92ef70bf6432cfdc9cf2c2546b2a3db8ca9705
ilhamgepe <gepedevelop@gmail.com>
2026-08-12 18:11:50 +0700

    add minimal password validation

--- src/main/java/com/gepe/app/auth/internal/dto/LoginRequest.java
+++ src/main/java/com/gepe/app/auth/internal/dto/LoginRequest.java
@@ -1,10 +1,11 @@
 package com.gepe.app.auth.internal.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
         @NotBlank @Email String email,
-        @NotBlank @Size(max = 72) String password) {
+        @NotBlank @Size(max = 72,min = 8) String password) {
 }
